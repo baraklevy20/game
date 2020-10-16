fd479bdf633cc9f0a8b4d4a3408e934fa8d6cf63
unknown <baraklevy20@gmail.com>
2020-10-16 10:26:55 +0300

    Updates

---
+++ precache-manifest.fa9d6bca423e6e1715cc90a88bef2960.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa686ac445c7c98c2cdef60433d4c10",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "ca2cce68fcce12d57ca8",
+    "url": "/game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "567390ee8effe79662cd",
+    "url": "/game/static/js/2.ec0eddb4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/game/static/js/2.ec0eddb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca2cce68fcce12d57ca8",
+    "url": "/game/static/js/main.086aaa4e.chunk.js"
+  },
+  {
+    "revision": "be523661599ed65828e4",
+    "url": "/game/static/js/runtime-main.eb6a8fc6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/game/static/media/logo.5d5d9eef.svg"
+  }
+]);
